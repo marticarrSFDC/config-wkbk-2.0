fc97a8cb3788e1b4d0bdaca96acab725cf96cd1f
marticarrSFDC <86790148+marticarrSFDC@users.noreply.github.com>
2025-03-18 17:09:48 -0400

    update action

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,14 +4,15 @@ module.exports = {
 	'!force-app/main/default/lwc/**/__tests__/**/*.*',
 	], // Adjust this path to match your source files
 	testRunner: 'mocha',
-	testRunnerNodeArgs: ["--require", "esm"],
+	testRunnerNodeArgs: ["--loader", "esm"],
 	mochaOptions: {
 	  	spec: ['force-app/main/default/lwc/**/__tests__/*.*',
 	'force-app/main/default/lwc/**/__tests__/**/*.*'], // Adjust this path to match your test files
-		require: ["ts-node/register"]
+		timeout: 10000, // Prevents tests from timing out
+		require: ["esm"]
 	},
 	reporters: ['clear-text', 'progress', 'html'],
-	coverageAnalysis: 'off', // Set to "perTest" if you want Stryker to optimize mutations
+	coverageAnalysis: 'perTest', // Set to "perTest" if you want Stryker to optimize mutations
 	thresholds: {
 		high: 100, // Desired mutation score
 		low: 90,  // Acceptable threshold
