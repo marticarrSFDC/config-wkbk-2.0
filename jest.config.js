dfe5256b9002fd90d049da37c89d85047c6b5c2e
marticarrSFDC <86790148+marticarrSFDC@users.noreply.github.com>
2025-03-18 16:06:21 -0400

    update action

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,6 @@ module.exports = {
         },
     },
     collectCoverage: true,
-    coverageReporters: ['text', 'lcov'],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'json', 'lcov'],
 };
