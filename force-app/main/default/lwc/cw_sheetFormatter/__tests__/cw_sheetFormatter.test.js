6b858fe870a698fd6d5b31b14d73fe64b8e49014
marticarrSFDC <86790148+marticarrSFDC@users.noreply.github.com>
2025-04-29 15:27:48 -0400

    fix tests

--- force-app/main/default/lwc/cw_sheetFormatter/__tests__/cw_sheetFormatter.test.js
+++ force-app/main/default/lwc/cw_sheetFormatter/__tests__/cw_sheetFormatter.test.js
@@ -10,7 +10,7 @@ describe('sheet-formatter', () => {
     it('generateSpreadsheet - calls SheetFormatter.generateSpreadsheet', () => {
         // Arrange
         const type = 'allobjects';
-        const spy = jest.spyOn(SheetFormatter.prototype, 'generateSpreadsheet').mockReturnValue({});
+        const spy = jest.spyOn(SheetFormatter, 'generateSpreadsheet').mockReturnValue({});
 
         // Act
         const sheet = SheetFormatter.generateSpreadsheet(type);
